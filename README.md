8ba987e5be6679e07dd99a76733c34c2e02bd6a2
lisophorm <lisophorm@gmail.com>
2017-03-17 14:52:03 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,16 @@
+**Beyond - Front End Developer Test**
+
+
+Candidate:**Alfonso Florio**
+
+Working url: **http://tom.crystal-bits.co.uk**
+
+Tested with: **Chrome and Firefox**
+
+
+
+
+_Do not hesitate to contact me if you have any questions_
+
+**07459 301326 - lisophorm@gmail.com**
+
--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('beyondEercise01', ['ngAnimate', 'ngCookies', 'ngSanitize', 'ngAria', 'ngResource', 'ui.router', 'ngMaterial', 'base64','ngMdIcons','ngSanitize','oblador.lazytube']);
+    .module('beyondEercise01', ['ngAnimate', 'ngCookies', 'ngSanitize', 'ngAria', 'ui.router', 'ngMaterial', 'base64', 'ngMdIcons', 'ngSanitize', 'oblador.lazytube']);
 
 })();
